107ab948e1ca8ada1558ab87e6c879638fdd3b11
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2023-01-24 18:57:37 +0530

    commit

--- Backend/src/model/note.js
+++ Backend/src/model/note.js
@@ -1,12 +1,39 @@
+// const mongoose = require("mongoose");
+
+// const noteschema = mongoose.Schema({
+//   id: {
+//     type: String,
+//     unique: true,
+//     required: true,
+//   },
+//   userId: {
+//     type: String,
+//     required: true,
+//   },
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   content: {
+//     type: String,
+//   },
+//   dateAdded: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// module.exports = mongoose.model("Note", noteschema);
+
 const mongoose = require("mongoose");
 
-const noteschema = mongoose.Schema({
+const noteSchema = mongoose.Schema({
   id: {
     type: String,
     unique: true,
     required: true,
   },
-  userId: {
+  userid: {
     type: String,
     required: true,
   },
@@ -17,10 +44,10 @@ const noteschema = mongoose.Schema({
   content: {
     type: String,
   },
-  dateAdded: {
+  dateadded: {
     type: Date,
     default: Date.now,
   },
 });
 
-module.exports = mongoose.model("Note", noteschema);
+module.exports = mongoose.model("Note", noteSchema);
--- Backend/src/routes/Note.js
+++ Backend/src/routes/Note.js
@@ -1,31 +1,64 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const Note = require(`./../model/note`);
+
+// router.post("/list", async (req, res) => {
+//   var notes = await Note.find({ userId: req.body.userid });
+//   res.json(notes);
+// });
+
+// router.post("/add", async (req, res) => {
+//   await Note.deleteOne({ id: req.body.id });
+
+//   const newNote = new Note({
+//     id: req.body.id,
+//     userId: req.body.userId,
+//     title: req.body.title,
+//     content: req.body.content,
+//   });
+//   await newNote.save();
+
+//   const response = { message: "New note created! " + `id: ${req.body.id}` };
+//   res.json(response);
+// });
+
+// router.post("/delete", async (req, res) => {
+//   await Note.deleteOne({ id: req.body.id });
+//   const response = { message: "Node Deleted " + `id: ${req.body.id}` };
+//   res.json(response);
+// });
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 
-const Note = require(`./../model/note`);
+const Note = require("./../model/Note");
 
-router.post("/list", async (req, res) => {
-  var notes = await Note.find({ userId: req.body.userid });
+router.post("/list", async function (req, res) {
+  var notes = await Note.find({ userid: req.body.userid });
   res.json(notes);
 });
 
-router.post("/add", async (req, res) => {
+router.post("/add", async function (req, res) {
   await Note.deleteOne({ id: req.body.id });
 
   const newNote = new Note({
     id: req.body.id,
-    userId: req.body.userId,
+    userid: req.body.userid,
     title: req.body.title,
     content: req.body.content,
   });
   await newNote.save();
 
-  const response = { message: "New note created! " + `id: ${req.body.id}` };
+  const response = { message: "New Note Created! " + `id: ${req.body.id}` };
   res.json(response);
 });
 
-router.post("/delete", async (req, res) => {
+router.post("/delete", async function (req, res) {
   await Note.deleteOne({ id: req.body.id });
-  const response = { message: "Node Deleted " + `id: ${req.body.id}` };
+  const response = { message: "Note Deleted! " + `id: ${req.body.id}` };
   res.json(response);
 });
 
